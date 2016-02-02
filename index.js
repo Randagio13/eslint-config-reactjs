4e45a16cc038adc66583fe3cebf7f4abf82b0f4c
Alessandro Casazza <imasterale@gmail.com>
2016-02-02 15:21:55 +0100

    Delete deprecate react/jsx-quotes

--- index.js
+++ index.js
@@ -149,7 +149,6 @@ module.exports = {
     "react/jsx-no-literals": 2,
     "react/jsx-no-undef": 2,
     "react/jsx-pascal-case": 2,
-    "react/jsx-quotes": 2,
     "react/jsx-sort-prop-types": 0,
     "react/jsx-sort-props": 0,
     "react/jsx-uses-react": 2,
